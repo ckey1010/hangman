6c3aaa9c91f20ff182b340f4615e3dd4550fed22
Colin Key <colin@ckey.me>
2015-10-31 22:51:13 -0600

    Find and replace halfway working
    Find and replace within the answer is working. There is an issue with
    finding and replacing multiple instances of letters. Additionally,
    removed the space after the underscore on line 5 of app.js and added
    letter spacing to CSS for the desired look.

--- hangman-app.js
+++ hangman-app.js
@@ -1,9 +1,8 @@
 var wordBank = ['apple', 'orange', 'banana', 'giraffe', 'colin', 'cherry', 'combo', 'reggae'];
 var answer = wordBank[Math.floor(Math.random()*wordBank.length)].toUpperCase();
 
-
-for (var i = 0; i < answer.length; i += 1) {
-	$('#puzzleAnswer').append('<span id="' + i +'">_ </span>');
+for (var i = 0; i < answer.length; i++) {
+	$('#puzzleAnswer').append('<span id="' + i +'">_</span>');
 }
 
 //When letters are clicked, replace underscore. indexOf letter within answer, use index to replace underscore with correctly guessed letter
@@ -13,9 +12,11 @@ $('.letters').click(function() {
 	var guess = $(this).text();
 	$(this).fadeOut('slow');
 	if (answer.indexOf(guess) >= 0 ) {
-		$('#puzzleAnswer').append(guess);
+		var puzzleAnswer = $('#puzzleAnswer').text();
+		var guessIndex = answer.indexOf(guess);
+		var newString = puzzleAnswer.substr(0, guessIndex) + guess + puzzleAnswer.substr(guessIndex+1); 
+		$('#puzzleAnswer').text(newString);
 	} else {
 		$('#usedLetters').append('<li>' + guess + '</li>').hide().fadeIn('slow');
-	}
+		}
 });
-
