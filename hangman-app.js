4112e9c7854f47f962022a68b2899fbf211e63be
Colin Key <colin@ckey.me>
2015-11-01 00:48:56 -0600

    Added canvas and draw functions
    The canvas element was added and the drawing functions added to app.js.
    Functions are not yet called in any way.

--- hangman-app.js
+++ hangman-app.js
@@ -1,22 +1,55 @@
 var wordBank = ['apple', 'orange', 'banana', 'giraffe', 'colin', 'cherry', 'combo', 'reggae'];
-var answer = wordBank[Math.floor(Math.random()*wordBank.length)].toUpperCase();
+var answer = wordBank[Math.floor(Math.random() * wordBank.length)].toUpperCase();
+var canvas = document.getElementById('canvas');
+canvas.setAttribute('width', '400');
+canvas.setAttribute('height', '400');
+var ctx = canvas.getContext('2d');
 
-for (var i = 0; i < answer.length; i++) {
-	$('#puzzleAnswer').append('<span id="' + i +'">_</span>');
+for (i = 0; i < answer.length; i++) {
+	$('#puzzleAnswer').append('<span id="' + i + '">_</span>');
 }
 
-//When letters are clicked, replace underscore. indexOf letter within answer, use index to replace underscore with correctly guessed letter
+function drawCanvas(startx, starty, endx, endy) {
+	ctx.beginPath()
+	ctx.strokeStyle = 'black';
+	ctx.moveTo(startx, starty);
+	ctx.lineTo(endx, endy);
+	ctx.stroke();
+}
+
+initializeCanvas = [drawCanvas(0, 375, 400, 375), drawCanvas(25,375,25,15), drawCanvas(25,15,200,15), drawCanvas(200,15,200,25)];
+
+
+function AnimateDude(bodyPart, startx, starty, endx, endy) {
+	this.bodyPart = bodyPart;
+	this.startx = startx;
+	this.starty = starty;
+	this.endx = endx;
+	this.endy = endy;
+}
+		
+drawHead = function() {
+	ctx.beginPath();
+	ctx.arc(200, 60, 35, 0, 2 * Math.PI);
+	ctx.stroke();
+};
+var drawTorso = new AnimateDude("Torso", 200, 95, 200, 220);
+var drawLeftLeg = new AnimateDude("Left Leg", 200, 220, 125, 290);
+var drawRightLeg = new AnimateDude("Right Leg", 200, 220, 275, 290);
+var drawLeftArm = new AnimateDude("Left Arm", 200, 145, 120, 120);
+var drawRightArm = new AnimateDude("Right Arm", 200, 145, 280, 120);
+
 
 //When .letters are clicked, 
 $('.letters').click(function() {
 	var guess = $(this).text();
 	$(this).fadeOut('slow');
-	if (answer.indexOf(guess) >= 0 ) {
+	if (answer.indexOf(guess) >= 0) {
 		var puzzleAnswer = $('#puzzleAnswer').text();
 		var guessIndex = answer.indexOf(guess);
 		var newString = puzzleAnswer.substr(0, guessIndex) + guess + puzzleAnswer.substr(guessIndex+1); 
 		$('#puzzleAnswer').text(newString);
 	} else {
 		$('#usedLetters').append('<li>' + guess + '</li>').hide().fadeIn('slow');
-		}
+	}
 });
